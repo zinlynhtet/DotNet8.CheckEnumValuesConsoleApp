5c18cf4aef41a9dc0e0525e3d35be776206155cc
Zin Linn Htet <mackk5504@gmail.com>
2024-04-25 00:37:59 +0630

    add Enum

--- DotNet8.CheckEnumValuesConsoleApp/Program.cs
+++ DotNet8.CheckEnumValuesConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DotNet8.CheckEnumValuesConsoleApp;
+
+public enum EnumService
+{
+    Apple,
+    Orange,
+    Lime,
+    GrapeFruit,
+    Banana,
+    StrawBerry
+}
+
+
