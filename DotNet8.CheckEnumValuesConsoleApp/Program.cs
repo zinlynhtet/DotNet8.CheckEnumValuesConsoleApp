d547df20801465f24316db34607dc963a5bebdbb
Zin Linn Htet <mackk5504@gmail.com>
2024-04-25 00:38:19 +0630

    Add Services

--- DotNet8.CheckEnumValuesConsoleApp/Program.cs
+++ DotNet8.CheckEnumValuesConsoleApp/Program.cs
@@ -10,4 +10,19 @@ public enum EnumService
     StrawBerry
 }
 
+class Program
+{
+    static void Main()
+    {
+        var names = Enum.GetValues(typeof(EnumService));
+        foreach (EnumService n in names)
+        {
+            Console.WriteLine(n);
+        }
 
+        var getNames = Enum.GetNames(typeof(EnumService)).Length;
+        var getValues = Enum.GetValues(typeof(EnumService)).Length;
+        Console.WriteLine("Total items by GetNames: " + getNames);
+        Console.WriteLine("Total items by GetValues: " + getValues);
+    }
+}
